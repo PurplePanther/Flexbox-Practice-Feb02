2382e143fbac85a2d7e49483f75cae4633bfe7a5
PurplePanther <45367470+PurplePanther@users.noreply.github.com>
2023-02-15 13:13:15 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* */} {/*<----That over there is how to comment JSX*/}
+
+      <div className="container-fluid">
+
+        <div className="box box_one">1</div>
+        <div className="box box_two">2</div>
+        <div className="box box_three">I am the Doctor and this is my spoon. I am the Doctor and this is my spoon.</div>
+        <div className="box box_four">4</div>
+        <div className="box box_five">5</div>
+        <div className="box box_six">6</div>
+
+        {/*<div>
+          <form>
+            <input type-text/>
+          </form>
+        </div>*/}
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
